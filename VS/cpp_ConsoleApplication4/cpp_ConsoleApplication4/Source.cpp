ace9f1f7dedb7d4322f36711421e9d55e19b0b43
allexandr <allexandr1980@gmail.com>
2015-12-17 21:20:25 +0300

    up

--- VS/cpp_ConsoleApplication4/cpp_ConsoleApplication4/Source.cpp
+++ VS/cpp_ConsoleApplication4/cpp_ConsoleApplication4/Source.cpp
@@ -6,6 +6,12 @@
 using namespace std;
 
 int main() {
-
-	return 0;
+	int a, b, c, d;
+	cin >> a >> b >> c >> d;
+	if (((a - 1 == c) && (b == d)) || ((a + 1 == c) && (b == d)) || ((a - 1 == c) && (b - 1 == d)) || ((a == c) && (b - 1 == d)) ||
+		((a + 1 == c) && (b - 1 == d)) || ((a - 1 == c) && (b + 1 == d)) || ((a == c) && (b + 1 == d)) || ((a + 1 == c) && (b + 1 == d)))
+		cout << "YES";
+	else
+		cout << "NO";
+	return 0;	
 }
