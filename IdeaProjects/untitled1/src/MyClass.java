e7a688a12c72d749e1aa0f7147e42ff70c7aa7bb
allexandr <allexandr1980@gmail.com>
2015-12-24 16:22:45 +0300

    up

---
+++ IdeaProjects/untitled1/src/MyClass.java
@@ -0,0 +1,16 @@
+/**
+ * Created by alex on 24.12.2015.
+ */
+public class MyClass {
+
+    public static void main (String args[]) {
+         int i = 6; double a = 0;
+         for (; i > 0; i--)
+             a = Math.sqrt(i);
+
+        	 System.out.println(a);
+        	}
+
+}
+
+
