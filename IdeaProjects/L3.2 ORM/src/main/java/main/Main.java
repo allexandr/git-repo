2e3e75aeb8735a75741e55dffca84e5a4792231e
allexandr <allexandr1980@gmail.com>
2015-12-26 15:14:05 +0300

    up

--- IdeaProjects/L3.2 ORM/src/main/java/main/Main.java
+++ IdeaProjects/L3.2 ORM/src/main/java/main/Main.java
@@ -1,60 +1,7 @@
 package main;
 
-
-import dbService.DBException;
-import dbService.DBService;
-import dbService.dataSets.UsersDataSet;
-import org.eclipse.jetty.server.Handler;
-import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.server.handler.HandlerList;
-import org.eclipse.jetty.server.handler.ResourceHandler;
-import org.eclipse.jetty.servlet.ServletContextHandler;
-import org.eclipse.jetty.servlet.ServletHolder;
-import servlets.SignInServlet;
-import servlets.SignUpServlet;
-
 /**
- * @author v.chibrikov
- *         <p>
- *         Пример кода для курса на https://stepic.org/
- *         <p>
- *         Описание курса и лицензия: https://github.com/vitaly-chibrikov/stepic_java_webserver
+ * Created by alex on 26.12.2015.
  */
 public class Main {
-    public static void main(String[] args) throws Exception{
-
-        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
-
-        context.addServlet(new ServletHolder(new SignInServlet(), "/signin");
-        context.addServlet(new ServletHolder(new SignUpServlet(), "/signup");
-
-        ResourceHandler resource_handler = new ResourceHandler();
-        resource_handler.setResourceBase("public_html");
-
-        HandlerList handlers = new HandlerList();
-        handlers.setHandlers(new Handler[]{resource_handler, context});
-
-        DBService dbService = new DBService();
-        dbService.printConnectInfo();
-
-        Server server = new Server(8080);
-        server.setHandler(handlers);
-
-
-        System.out.println("Server started!");
-        server.start();
-        server.join();
-
-/*
-        try {
-            long userId = dbService.addUser("ss", "ss");
-            System.out.println("Added user id: " + userId);
-
-            UsersDataSet dataSet = dbService.getUser(userId);
-            System.out.println("User data set: " + dataSet);
-
-        } catch (DBException e) {
-            e.printStackTrace();
-        } */
-    }
 }
