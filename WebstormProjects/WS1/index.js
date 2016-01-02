2dd6a81615a6ee31936abc88430e13ce9edfbd1f
allexandr <allexandr1980@gmail.com>
2016-01-02 21:58:53 +0300

    up

--- WebstormProjects/WS1/index.js
+++ WebstormProjects/WS1/index.js
@@ -1,14 +1,15 @@
 /**
  * Created by alex on 29.12.2015.
  */
-for (var i = 1; i < 50; i++) {
-    if (i % 15 === 0) {
-        document.write("FooBaz ");
-    } else if (i % 3 === 0) {
-        document.write("Foo ");
-    } else if (i % 5 === 0){
-        document.write("Baz ");
-    } else  {
-        document.write(i + " ");
-    }
+function listener(){
+    alert("По мне кликнули");
 }
+window.addEventListener("load", function(){
+    var div = document.querySelector(".c1");
+    div.addEventListener("click", listener);
+    div.onclick = function(){
+        div.removeEventListener("click", listener);
+    }
+});
+
+
