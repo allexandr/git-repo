3446cd772e047028e25d11468f08dc6212995337
allexandr <allexandr1980@gmail.com>
2016-02-03 16:52:48 +0300

    upup

--- WebstormProjects/WS1/index.js
+++ WebstormProjects/WS1/index.js
@@ -1,15 +1,13 @@
 /**
  * Created by alex on 29.12.2015.
  */
-function listener(){
-    alert("По мне кликнули");
-}
-window.addEventListener("load", function(){
-    var div = document.querySelector(".c1");
-    div.addEventListener("click", listener);
-    div.onclick = function(){
-        div.removeEventListener("click", listener);
+function truncate(st, maxlength){
+    if (st.length > maxlength){
+        return st.slice(0, maxlength - 3) + "...";
     }
-});
+    return st;
+}
+
+alert(truncate("Вот, что мне хотелось бы сказать на эту тему:", 20));
 
 
