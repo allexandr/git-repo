c3473b3d1199474829b506c4d63529af027efad2
allexandr <allexandr1980@gmail.com>
2016-02-27 13:12:25 +0300

    up

--- WebstormProjects/WS1/index.js
+++ WebstormProjects/WS1/index.js
@@ -1,15 +1,2 @@
-arr = [ 1, 2, 3, 4, 5 ];
 
-function getSums(arr) {
-    var outArr = [];
-    arr.reduce(function(sum, current){
-        outArr.push(sum + current);
-    });
-
-    return outArr;
-}
-
-
-
-alert( getSums( arr )); // 4,5,2,5
 
